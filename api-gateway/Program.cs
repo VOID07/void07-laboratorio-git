83b51421c2487f3304f0dc6e59071e8af8113095
void07 <dagoDocker@dagoDocker.hestiwnzfwqudikksuccqcrnsc.cx.internal.cloudapp.net>
2021-08-19 06:01:51 +0000

    Jira-1234

--- api-gateway/Program.cs
+++ api-gateway/Program.cs
@@ -10,3 +10,5 @@ namespace api_gateway
         }
     }
 }
+
+// Copyright
---
+++ api-gateway/src/Code.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace api_gateway
+{
+    class Code
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+// Copyright
